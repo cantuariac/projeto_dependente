a41d786b1eb5b00dd45191490d56b1137e2be167
Caio Cantuária <caio.cantuaria@gmail.com>
2022-03-22 15:08:25 -0300

    usando numpy

--- code.py
+++ code.py
@@ -2,19 +2,27 @@ from colored import fore, style
 from PIL import Image
 import requests
 from io import BytesIO
+import numpy as np
 
 print(fore.GREEN)
 print('If I have vulnerable code you ask?', style.RESET)
 input('<press enter>')
 
-print(fore.GREEN)
+print(fore.RED)
 print(f'Well, it {style.BOLD} depends', style.RESET)
 input('<press enter>')
 
-print(fore.GREEN)
+print(fore.BLUE)
 print('Here is an image...', style.RESET)
-input('<press enter>')
-
 response = requests.get('https://i.kym-cdn.com/entries/icons/mobile/000/025/817/Screen_Shot_2018-03-30_at_11.34.27_AM.jpg')
 img = Image.open(BytesIO(response.content))
 img.show()
+input('<press enter>')
+
+img = np.array(img)
+print(img)
+print(fore.YELLOW)
+print('I turned it into an array for no reason', style.RESET)
+input('<press enter>')
+
+print('Good bye!')
